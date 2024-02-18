8038d544272a985ecf78c8d155104b273c358e41
sampad <sampad@storymirror.com>
2024-02-18 13:05:59 +0530

    Add Loader Component

--- src/contexts/commonContext.tsx
+++ src/contexts/commonContext.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { createContext, useContext, useState } from "react";
+import Loader from '../app/components/Loader';
 
 interface contextProps {
     isLoading: boolean,
@@ -22,28 +23,10 @@ export const CommonContextProvider = ({ children }: { children: React.ReactNode 
 
     return (
         <CommonContext.Provider value={contextValue}>
-            { isLoading && 
-                <div className="w-full h-full fixed left-0 right-0 top-0">
-                    <div className="w-full bg-white h-full opacity-50"></div>
-                    <div className="absolute left-0 right-0 top-0 text-center w-full">
-                        <span className="flex justify-center items-center h-screen">
-                            <h1 className="text-2xl font-bold">Loading...</h1>
-                        </span>
-                    </div>
-                </div>
-            }
+            <Loader />
             {children}
         </CommonContext.Provider>
     )
 }
 
 export const useCommonContext = () => useContext(CommonContext);
-
-// export const useCommonContext = () => {
-//     const context = useContext(CommonContext);
-//     if (!context) {
-// 		throw new Error('useCommonContext must be used within a CommonContextProvider');
-// 	}
-
-// 	return context;
-// }
